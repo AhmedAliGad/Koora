8950f269cbaa243eca21d661e7ecd30a63c2e5c7
dena elsaid <dena.work2018@gmail.com>
2023-05-14 22:18:29 +0200

    Add Hero Section in profile page

--- resources/views/front/pages/profile.blade.php
+++ resources/views/front/pages/profile.blade.php
@@ -9,7 +9,7 @@
 <section class="hero inner-header">
 	<div class="section-padding position-relative club-section">
 		<div class="container">
-			<div class="columns">
+			<div class="columns is-multiline d-block">
 				<div class="column is-2 pl-6">
 					<figure class="image card has-text-centered" data-aos="zoom-in" data-aos-delay="300">
 						<lazy-load
@@ -21,7 +21,67 @@
 						class="image-contain"
 						/>
 					</figure>
-					<h1 class="is-size-2 has-text-white mt-3" data-aos="slide-up" data-aos-delay="300">الأهلي</h1>
+				</div>
+				<div class="columns is-multiline">
+					<div class="column is-3">
+						<h1 class="pr-2 is-size-3 has-text-white mt-1" data-aos="slide-up" data-aos-delay="300">الأهلي</h1>
+						<ul class="socials mb-5 justify-content-start has-text-right d-block mt-3" data-aos="slide-up" data-aos-delay="300">
+							<li class="d-inline-block">
+								<a class="hvr-rectangle-out" href="#">
+									<i class="fa-brands fa-facebook-f"></i>
+								</a>
+							</li>
+							<li class="d-inline-block">
+								<a class="hvr-rectangle-out" href="#">
+									<i class="fa-brands fa-twitter"></i>
+								</a>
+							</li>
+							<li class="d-inline-block">
+								<a class="hvr-rectangle-out" href="#">
+									<i class="fa-brands fa-youtube"></i>
+								</a>
+							</li>
+							<li class="d-inline-block">
+								<a class="hvr-rectangle-out" href="#">
+									<i class="fa-brands fa-instagram"></i>
+								</a>
+							</li>
+						</ul>
+						<a href="#" class="club-link" data-aos="slide-up" data-aos-delay="300">
+							<i class="has-text-gold fa-solid fa-up-right-from-square ml-1"></i>
+							<span>https://alahli.qa/</span>
+						</a>
+					</div>
+					<div class="column is-6">
+						<div class="club-contact p-3 py-5 mt-6">
+							<div class="columns is-multiline">
+								<div class="column is-6">
+									<a href="#" class="has-text-primary" data-aos="slide-up" data-aos-delay="300">
+										<i class="has-text-gold fa-brands fa-whatsapp ml-1"></i>
+										<span>010898738</span>
+									</a>
+								</div>
+								<div class="column is-6">
+									<a href="#" class="has-text-primary" data-aos="slide-up" data-aos-delay="300">
+										<i class="has-text-gold fa-solid fa-phone-volume ml-1"></i>
+										<span>0986765</span>
+									</a>
+								</div>
+								<div class="column is-6">
+									<a href="#" class="has-text-primary" data-aos="slide-up" data-aos-delay="300">
+										<i class="has-text-gold fa-solid fa-location-dot ml-1"></i>
+										<span>استاد القاهرة - نادى  الأهلى الرياضى</span>
+									</a>
+								</div>
+								<div class="column is-6">
+									<a href="#" class="has-text-primary" data-aos="slide-up" data-aos-delay="300">
+										<i class="has-text-gold fa-solid fa-envelope-open-text ml-1"></i>
+										<span>test@gmail.com</span>
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
