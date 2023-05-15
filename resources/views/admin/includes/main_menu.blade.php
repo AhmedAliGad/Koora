06bbbaaf5f590f2d6884f1eadba645f032dfbdbc
Ahmed Gad <ahmed.aligad@fndevcs.com>
2023-05-15 08:19:22 +0300

    Add Fav To admin

--- resources/views/admin/includes/main_menu.blade.php
+++ resources/views/admin/includes/main_menu.blade.php
@@ -27,12 +27,12 @@
         <a class="link-item" href="{{ route('admin.photo_galleries.create') }}">Add Photo Gallery</a>
         <a class="link-item" href="{{ route('admin.photo_galleries.index') }}">photo Galleries List</a>
     </collapse-item>
-    <collapse-item title="Manage App Settings" icon="fa fa-cogs">
-        <a class="link-item" href="{{ route('admin.cities.index') }}">Cities List</a>
-        <a class="link-item" href="{{ route('admin.working_hours.index') }}">Working Hours</a>
+<!--    <collapse-item title="Manage App Settings" icon="fa fa-cogs">
         <a class="link-item" href="{{ route('admin.settings.edit') }}">General Settings</a>
-    </collapse-item>
+    </collapse-item>-->
+<!--
     <a href="{{ route('admin.contact_messages.index') }}" class="card link-item-no-collapse"><i class="fa fa-envelope"></i><span>Contact Messages</span></a>
+-->
     <a href="{{ route('admin_logout') }}" onclick="event.preventDefault();
   document.getElementById('header_logout').submit();" class="card link-item-no-collapse"><i
             class="fas fa-sign-out-alt"></i><span>Logout</span></a>
