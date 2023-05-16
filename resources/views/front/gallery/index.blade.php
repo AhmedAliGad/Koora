1c804679806abfad0ad369675503a137345af740
dena elsaid <dena.work2018@gmail.com>
2023-05-16 22:37:09 +0200

    Edit Title in gallery page

--- resources/views/front/gallery/index.blade.php
+++ resources/views/front/gallery/index.blade.php
@@ -9,7 +9,7 @@
 <section class="hero inner-header mt-78">
 	<div class="section-padding position-relative club-section">
 		<div class="container has-text-centered">
-			<h1 class="pr-2 mt-6 is-size-3 has-bottom-border has-text-secondary has-text-centered d-inline-block mx-auto" data-aos="zoom-in" data-aos-delay="300">المركز الإعلامى</h1>
+			<h1 class="pr-2 mt-6 is-size-3 has-bottom-border has-text-secondary has-text-centered d-inline-block mx-auto" data-aos="zoom-in" data-aos-delay="300">معرض الصور</h1>
 		</div>
 	</div>
 </section>
