96c8705928ea059eb180a0755eae71d07baa1578
dena elsaid <dena.work2018@gmail.com>
2023-05-18 04:10:23 +0200

    Edit Inner Header Section

--- resources/views/front/gallery/index.blade.php
+++ resources/views/front/gallery/index.blade.php
@@ -9,7 +9,9 @@
 <section class="hero inner-header mt-78">
 	<div class="section-padding position-relative club-section">
 		<div class="container has-text-centered">
-			<h1 class="pr-2 mt-6 is-size-3 has-bottom-border has-text-secondary has-text-centered d-inline-block mx-auto" data-aos="zoom-in" data-aos-delay="300">معرض الصور</h1>
+			<a href="{{ route('gallery') }}">
+				<h1 class="pr-2 mt-6 is-size-3 has-bottom-border has-text-secondary has-text-centered d-inline-block mx-auto" data-aos="zoom-in" data-aos-delay="300">معرض الصور</h1>
+			</a>
 		</div>
 	</div>
 </section>
--- resources/views/front/gallery/show.blade.php
+++ resources/views/front/gallery/show.blade.php
@@ -9,7 +9,9 @@
 <section class="hero inner-header mt-78">
 	<div class="section-padding position-relative club-section">
 		<div class="container has-text-centered">
-			<h1 class="pr-2 mt-6 is-size-3 has-bottom-border has-text-secondary has-text-centered d-inline-block mx-auto" data-aos="zoom-in" data-aos-delay="300">معرض الصور</h1>
+			<a href="{{ route('gallery') }}">
+				<h1 class="pr-2 mt-6 is-size-3 has-bottom-border has-text-secondary has-text-centered d-inline-block mx-auto" data-aos="zoom-in" data-aos-delay="300">معرض الصور</h1>
+			</a>
 		</div>
 	</div>
 </section>
@@ -17,10 +19,12 @@
 <section class="gallery-page section-padding has-text-centered has-background-grey-lighter">
 	<div class="container">
 		<div class="card box-shadow p-4">
-			<h4 data-aos="zoom-in" data-aos-delay="300" class="has-text-gold mb-5">
-				<i class="fa-solid fa-camera-retro has-text-secondary is-size-5 ml-2"></i>
-				5 ألقاب.. سيدات سلة الأهلى يحققن "العلامة الكاملة" ويحصدن كل البطولات
-			</h4>
+			<a href="{{ route('gallery') }}">
+				<h4 data-aos="zoom-in" data-aos-delay="300" class="has-text-gold mb-5">
+					<i class="fa-solid fa-camera-retro has-text-secondary is-size-5 ml-2"></i>
+					5 ألقاب.. سيدات سلة الأهلى يحققن "العلامة الكاملة" ويحصدن كل البطولات
+				</h4>
+			</a>
 			<div class="columns is-multiline is-centered" dir="ltr">
 				<div class="column is-9">
 					<slick class="gallery-slider" ref="slick"  :options="{ slidesToShow: 1, slidesToScroll: 1, dots:false, arrows: true, asNavFor: '.gallery-nav', swipe: true  , loop: true, infinite: true, autoplay: true,
@@ -30,66 +34,66 @@
 						settings: {
 						arrows: false,
 					}
-					},
-					]
+				},
+				]
 
 
-				}">
-				<div class="item box-shadow radius-15">
-						<figure class="w-100 full-height image position-relative">
-							<img src="/front/images/1.jpg" class="box-shadow radius-15" ss="image-cover radius-15"alt="">
-						</figure>
-					
-				</div>
-				<div class="item box-shadow radius-15">
-		
-						<figure class="box-shadow radius-15 w-100 full-height image position-relative">
-							<img src="/front/images/2.jpg" class="image-cover radius-15"alt="">
-						</figure>
-					
-				</div>
-				<div class="item box-shadow radius-15">
-		
-						<figure class="box-shadow radius-15 w-100 full-height image position-relative">
-							<img src="/front/images/1.jpg" class="image-cover radius-15"alt="">
-						</figure>
-					
-				</div>
-				<div class="item box-shadow radius-15">
-		
-						<figure class="box-shadow radius-15 w-100 full-height image position-relative">
-							<img src="/front/images/2.jpg" class="image-cover radius-15"alt="">
-						</figure>
-					
-				</div>
-				<div class="item box-shadow radius-15">
-		
-						<figure class="box-shadow radius-15 w-100 full-height image position-relative">
-							<img src="/front/images/1.jpg" class="image-cover radius-15"alt="">
-						</figure>
-					
-				</div>
-				<div class="item box-shadow radius-15">
-		
-						<figure class="box-shadow radius-15 w-100 full-height image position-relative">
-							<img src="/front/images/2.jpg" class="image-cover radius-15"alt="">
-						</figure>
-					
-				</div>
-				<div class="item box-shadow radius-15">
-		
-						<figure class="box-shadow radius-15 w-100 full-height image position-relative">
-							<img src="/front/images/1.jpg" class="image-cover radius-15"alt="">
-						</figure>
-					
-				</div>
-				<div class="item box-shadow radius-15">
-		
-						<figure class="box-shadow radius-15 w-100 full-height image position-relative">
-							<img src="/front/images/2.jpg" class="image-cover radius-15"alt="">
-						</figure>
-					
-				</div>
+			}">
+			<div class="item box-shadow radius-15">
+				<figure class="w-100 full-height image position-relative">
+					<img src="/front/images/1.jpg" class="box-shadow radius-15" ss="image-cover radius-15"alt="">
+				</figure>
+
+			</div>
+			<div class="item box-shadow radius-15">
+
+				<figure class="box-shadow radius-15 w-100 full-height image position-relative">
+					<img src="/front/images/2.jpg" class="image-cover radius-15"alt="">
+				</figure>
+
+			</div>
+			<div class="item box-shadow radius-15">
+
+				<figure class="box-shadow radius-15 w-100 full-height image position-relative">
+					<img src="/front/images/1.jpg" class="image-cover radius-15"alt="">
+				</figure>
+
+			</div>
+			<div class="item box-shadow radius-15">
+
+				<figure class="box-shadow radius-15 w-100 full-height image position-relative">
+					<img src="/front/images/2.jpg" class="image-cover radius-15"alt="">
+				</figure>
+
+			</div>
+			<div class="item box-shadow radius-15">
+
+				<figure class="box-shadow radius-15 w-100 full-height image position-relative">
+					<img src="/front/images/1.jpg" class="image-cover radius-15"alt="">
+				</figure>
+
+			</div>
+			<div class="item box-shadow radius-15">
+
+				<figure class="box-shadow radius-15 w-100 full-height image position-relative">
+					<img src="/front/images/2.jpg" class="image-cover radius-15"alt="">
+				</figure>
+
+			</div>
+			<div class="item box-shadow radius-15">
+
+				<figure class="box-shadow radius-15 w-100 full-height image position-relative">
+					<img src="/front/images/1.jpg" class="image-cover radius-15"alt="">
+				</figure>
+
+			</div>
+			<div class="item box-shadow radius-15">
+
+				<figure class="box-shadow radius-15 w-100 full-height image position-relative">
+					<img src="/front/images/2.jpg" class="image-cover radius-15"alt="">
+				</figure>
+
+			</div>
 		</slick>
 	</div>
 	<div class="column is-12" dir="ltr">
@@ -100,59 +104,59 @@
 			settings: {
 			slidesToShow: 3
 		}
-		},
-		{
-			breakpoint: 375,
-			settings: {
-			slidesToShow: 2
-		}
-		},
-		]
-		}">
-			<div class="item box-shadow radius-15">
-				<figure class="w-100 full-height image position-relative">
-					<img src="/front/images/1.jpg" class="box-shadow radius-15" ss="image-cover radius-15"alt="">
-				</figure>
-			</div>
-			<div class="item box-shadow radius-15">
+	},
+	{
+		breakpoint: 375,
+		settings: {
+		slidesToShow: 2
+	}
+},
+]
+}">
+<div class="item box-shadow radius-15">
+	<figure class="w-100 full-height image position-relative">
+		<img src="/front/images/1.jpg" class="box-shadow radius-15" ss="image-cover radius-15"alt="">
+	</figure>
+</div>
+<div class="item box-shadow radius-15">
 	
-					<figure class="box-shadow radius-15 w-100 full-height image position-relative">
-						<img src="/front/images/2.jpg" class="image-cover radius-15"alt="">
-					</figure>
-				
-			</div>
-			<div class="item box-shadow radius-15">
-				<figure class="box-shadow radius-15 w-100 full-height image position-relative">
-					<img src="/front/images/1.jpg" class="image-cover radius-15"alt="">
-				</figure>
-			</div>
-			<div class="item box-shadow radius-15">
-				<figure class="box-shadow radius-15 w-100 full-height image position-relative">
-					<img src="/front/images/2.jpg" class="image-cover radius-15"alt="">
-				</figure>
-			</div>
-			<div class="item box-shadow radius-15">
-				<figure class="box-shadow radius-15 w-100 full-height image position-relative">
-					<img src="/front/images/1.jpg" class="image-cover radius-15"alt="">
-				</figure>
-			</div>
-			<div class="item box-shadow radius-15">
-				<figure class="box-shadow radius-15 w-100 full-height image position-relative">
-					<img src="/front/images/2.jpg" class="image-cover radius-15"alt="">
-				</figure>
-			</div>
-			<div class="item box-shadow radius-15">
-				<figure class="box-shadow radius-15 w-100 full-height image position-relative">
-					<img src="/front/images/1.jpg" class="image-cover radius-15"alt="">
-				</figure>
-			</div>
-			<div class="item box-shadow radius-15">
-				<figure class="box-shadow radius-15 w-100 full-height image position-relative">
-					<img src="/front/images/2.jpg" class="image-cover radius-15"alt="">
-				</figure>
-			</div>
-		</slick>
-	</div>
+	<figure class="box-shadow radius-15 w-100 full-height image position-relative">
+		<img src="/front/images/2.jpg" class="image-cover radius-15"alt="">
+	</figure>
+
+</div>
+<div class="item box-shadow radius-15">
+	<figure class="box-shadow radius-15 w-100 full-height image position-relative">
+		<img src="/front/images/1.jpg" class="image-cover radius-15"alt="">
+	</figure>
+</div>
+<div class="item box-shadow radius-15">
+	<figure class="box-shadow radius-15 w-100 full-height image position-relative">
+		<img src="/front/images/2.jpg" class="image-cover radius-15"alt="">
+	</figure>
+</div>
+<div class="item box-shadow radius-15">
+	<figure class="box-shadow radius-15 w-100 full-height image position-relative">
+		<img src="/front/images/1.jpg" class="image-cover radius-15"alt="">
+	</figure>
+</div>
+<div class="item box-shadow radius-15">
+	<figure class="box-shadow radius-15 w-100 full-height image position-relative">
+		<img src="/front/images/2.jpg" class="image-cover radius-15"alt="">
+	</figure>
+</div>
+<div class="item box-shadow radius-15">
+	<figure class="box-shadow radius-15 w-100 full-height image position-relative">
+		<img src="/front/images/1.jpg" class="image-cover radius-15"alt="">
+	</figure>
+</div>
+<div class="item box-shadow radius-15">
+	<figure class="box-shadow radius-15 w-100 full-height image position-relative">
+		<img src="/front/images/2.jpg" class="image-cover radius-15"alt="">
+	</figure>
+</div>
+</slick>
+</div>
 </div>
 
 			{{-- <div dir="ltr" id="lightgallery">
